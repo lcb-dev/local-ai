69cb612011da6ada746b25511a86f774c00e9c34
Juskiie <liam@lcb-dev.biz>
2025-10-31 14:31:39 +0000

    Setup basic logging in main file. Created todo list.

--- src/local_ai/main.py
+++ src/local_ai/main.py
@@ -1,7 +1,16 @@
+import logging
 
+logger = logging.getLogger(__name__)
 
 def main():
-    pass
+    logging.basicConfig(
+        filename="local_ai.log", 
+        format='(%(asctime)s) [%(filename)s:%(funcName)s:%(levelname)s]:: %(message)s', 
+        datefmt='%d/%m/%Y %I:%M:%S %p', 
+        level=logging.INFO)
+    logger.info("Started local_ai!")
+    # Do stuff
+    logger.info("Finished!")
 
 if __name__ == '__main__':
     main()
